1c30d553576a5c9f4b12fb95306528db75e381c3
Ivo Wetzel <ivo.wetzel@googlemail.com>
2015-07-22 02:10:09 +0200

    Split out lexers into distinct files

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,9 +3,11 @@ pub use self::token::Token;
 pub use self::token::TokenType;
 pub use self::expression::Expression;
 pub use self::lexer::Lexer;
+pub use self::base_lexer::BaseLexer;
 
 mod operator;
 mod token;
 mod expression;
+mod base_lexer;
 mod lexer;
 
